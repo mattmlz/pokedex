53dfefb36633ee8e06357b7d2c025b3cdc0d4a61
mattmlz <matthieu.toussaint@hetic.net>
2018-06-03 19:23:03 +0200

    :construction: starting to add config
    starting to add config #1

---
+++ src/public/index.php
@@ -0,0 +1,31 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+//Set usefulls variables
+$config['displayErrorDetails'] = true;
+$config['db']['host']   = "localhost";
+$config['db']['user']   = "user";
+$config['db']['pass']   = "password";
+$config['db']['dbname'] = "hetic_pokedex";
+
+$app = new \Slim\App(["settings" => $config]);
+$container = $app->getContainer();
+
+$container['view'] = new \Slim\Views\PhpRenderer("../templates/");
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
+        $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
