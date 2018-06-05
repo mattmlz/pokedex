b34d686c9bd7e1424eb916514d57c9d27579cff0
mattmlz <matthieu.toussaint@hetic.net>
2018-06-05 12:34:47 +0200

    :construction: Working on base of website
    Working on base of website #1

--- app/routes.php
+++ app/routes.php
@@ -16,3 +16,20 @@ $app->get('/login',function(Request $request, Response $response) {
   $dataView = [];
   return $this->view->render($response, 'pages/login.twig', $dataView);
 })->setName('login');
+
+// Check login in DB and log or not
+/* $app->post('/login/log', function(Request $request, Response $response) {
+  $data = $request->getParsedBody();
+  $user_data = [];
+  $user_data['email'] = filter_var($data['email'], FILTER_SANITIZE_STRING);
+  $user_data['password'] = filter_var($data['password'], FILTER_SANITIZE_STRING);
+  echo '<pre>';
+  var_dump($user_data);
+  echo '</pre>';
+}); */
+
+// Sign in
+$app->get('/sign-in',function(Request $request, Response $response) {
+  $dataView = [];
+  return $this->view->render($response, 'pages/sign-in.twig', $dataView);
+})->setName('sign-in');
