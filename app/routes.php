fa3db15ece88d514807e7ec3aa68f31d59031a6d
mattmlz <matthieu.toussaint@hetic.net>
2018-06-05 10:51:30 +0200

    :memo: creating a static login page
    creating a static login page #1

--- app/routes.php
+++ app/routes.php
@@ -4,8 +4,15 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+/* ROUTES */
 // Home
 $app->get('/',function(Request $request, Response $response) {
   $dataView = [];
   return $this->view->render($response, 'pages/home.twig', $dataView);
 })->setName('home');
+
+// Login
+$app->get('/login',function(Request $request, Response $response) {
+  $dataView = [];
+  return $this->view->render($response, 'pages/login.twig', $dataView);
+})->setName('login');
