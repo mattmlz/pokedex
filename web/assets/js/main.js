ec6686f0738aa3d6152ee3f3a2ffd64df33d9bdf
mattmlz <matthieu.toussaint@hetic.net>
2018-06-09 19:34:06 +0200

    :recycle: move js in a independant file
    move js in a independant file

---
+++ web/assets/js/main.js
@@ -0,0 +1,5 @@
+// Initialize text input in list.twig
+document.addEventListener('DOMContentLoaded', function() {
+  var elem = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elem);
+});
